ea4521f6ca4855bd18b538b33eb8f545ac731b8b
Giirotto <raffa.girotto@gmail.com>
2022-04-12 13:08:48 -0300

    loops

---
+++ ruby/ruby-basico/types.rb
@@ -0,0 +1,17 @@
+
+# -- Tipos Ruby --
+
+product     = "Macbook Air"
+price       = 5999.99
+theAmount   = 10
+available   = true
+
+puts product.class
+puts price.class
+puts theAmount.class
+puts available.class
+
+puts product
+puts price
+puts theAmount
+puts available
